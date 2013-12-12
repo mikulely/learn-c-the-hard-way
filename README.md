ba8346960ce0946e99573baeda90778308c6df00
Jiaying Ren <mikulely@gmail.com>
2013-12-12 16:59:37 +0800

    Add io related demo

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Code snippet retyped from [learn c the hard way](http://c.learncodethehardway.or
 + prototype-based object system base on ex19
 + Zed's awesome debug macros (2013-12-11 11:17 AM)
 + extern var and scope-related demo (2013-12-11 3:59 PM)
++ io related function demo (2013-12-12 11:25 AM)
---
+++ io/io.c
@@ -0,0 +1,71 @@
+// file name : io.c
+// description : another memorization execise for io
+// created at: Thu 12 Dec 2013 11:25:59 AM CST
+
+#include <stdio.h>
+#include "../dbg/dbg.h"
+
+#define MAX_DATA 100
+
+typedef enum EyeColor{
+        BLUE_EYES, GREEN_EYES, BROWN_EYES,
+        BLACK_EYES, OTHER_EYES
+} EyeColor;
+
+const char* EYE_COLOR_NAMES[] = {
+        "Blue", "Green", "Brown", "Black", "Other"
+};
+
+typedef struct Person{
+        int age;
+        char first_name[MAX_DATA];
+        char last_name[MAX_DATA];
+        EyeColor eyes;
+        float income;
+} Person;
+
+int main(){
+        Person you = {.age = 0};
+        char *in = NULL;
+        printf("What's your First Name?");
+        // do not use get,caz it has no idea how big the input buffer is at all.
+        in = fgets(you.first_name, MAX_DATA - 1, stdin);
+        check(in != NULL, "Failed to read first name.");
+
+        printf("What's your Last Name?");
+        in = fgets(you.last_name, MAX_DATA - 1, stdin);
+        check(in != NULL, "Failed to read last name.");
+
+        printf("How old are you?");
+        int rc = fscanf(stdin, "%d", &you.age);
+        check(rc > 0, "You need to enter a number.");
+
+        int i = 0;
+        printf("What color are your eyes:\n");
+        for(i = 0; i <= OTHER_EYES; i++){
+                printf("%d) %s\n", i+1, EYE_COLOR_NAMES[i]);
+        }
+        printf("> ");
+
+        int eyes = -1;
+        rc = fscanf(stdin, "%d", &eyes);
+        check(rc > 0, "You need to enter a number.");
+
+        you.eyes = eyes - 1;
+        check(you.eyes <= OTHER_EYES && you.eyes >= 0, "Do it right, that's not an option");
+
+        printf("How much do you make a hour?");
+        rc = fscanf(stdin, "%f", &you.income);
+        check(rc > 0, "Enter a floating point number");
+
+        printf("---Result---\n");
+        printf("First name: %s", you.first_name);
+        printf("Last name: %s", you.last_name);
+        printf("Age: %d\n", you.age);
+        printf("Eyes: %s\n", EYE_COLOR_NAMES[you.eyes]);
+        printf("Income: %f\n", you.income);
+
+        return 0;
+error:
+        return -1;
+}
